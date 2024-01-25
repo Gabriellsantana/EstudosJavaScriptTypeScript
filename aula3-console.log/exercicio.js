61a6fe1a3bf9866c77ab8285813d6d82827a5fa6
Gabriel <gabrieldevspace@gmail.com>
2024-01-25 11:39:42 -0300

    variável varR,let

---
+++ aula3-console.log/exercicio.js
@@ -0,0 +1,2 @@
+console.log("Meu nome é 'Gabriel' e estou apredendo javaScript as ",12," da noite");
+console.log(`Meu nome "Gabriel" estou apredendo JS as ${12} madrugada`);
---
+++ aula6-var&let/index.js
@@ -0,0 +1,21 @@
+// REGRAS:
+// Não podemos criar variáveis com palvras reseverdas
+// Variáveis precisam ter nomes significativos
+// Não podem conter espaços ou traços
+// Utilizamos camelcase
+// Case-sensitive
+// Não podemos redeclarar variáveis com let
+// Não utilize VAR , utilize LET
+
+let nome = "Gabriel"; //let é mais ultilizado var é algo antigo pouco usado
+var sobrenome = "Santana"
+let idade = 20
+let cargo = "Programador Sênio"
+let linguagem = "Java Script"
+let vazio = null //vc poder cria variavel vazia no js e execultar 
+let vazio2; 
+
+
+console.log(`Ola me chamo ${nome} ${sobrenome}`)
+console.log(`tenho ${idade} anos`)
+console.log(`Estou Atualmente no cargo de ${cargo} no Google trabalhando com a linguagem ${linguagem}`)
