ecb1160af88e5f371dce142247f9b6090beca0c6
Gabriel <gabrieldevspace@gmail.com>
2024-01-24 23:58:56 -0300

    console.log()

---
+++ aula3-console.log/index.js
@@ -0,0 +1,14 @@
+console.log("Hello Word");
+
+var nome = "Gabriel"
+
+console.log(`Meu nome é ${nome}`);
+
+console.log(typeof(nome)); //imprimindo na tela o tipo da variavel com typeof
+
+console.log(1, 2, 3, 4, 5, 6);
+console.log(4+2)
+console.log(4/2)
+console.log(4*2)
+console.log(4-2)
+console.log(4%2)
