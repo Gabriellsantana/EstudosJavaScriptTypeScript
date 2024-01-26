9a9580f9f793ce78e65f22b1b95a6f212369a281
Gabriel <gabrieldevspace@gmail.com>
2024-01-25 23:56:56 -0300

    operadores aritimeticos

---
+++ aula10-aritimeticos/index.js
@@ -0,0 +1,26 @@
+// aritimeticos
+// + adição 
+// * multiplicação 
+// / divisão
+// - subtração
+// % resto da divisão
+// ordem  (); ** ;  * ; / ; % ; + ; -
+// incremento = ++
+// decremento = --
+
+let a = 10;
+let b = 20;
+
+console.log(`Soma: ${a} + ${b} = ${a+b}`);
+console.log(`Multiplicação: ${a} x ${b} = ${a*b}`);
+console.log(`Divisão: ${a} / ${b} = ${a/b}`);
+console.log(`Subtração: ${a} - ${b} = ${a-b}`);
+console.log(`Resto da divisão: ${a} % ${b} = ${a%b}`);
+console.log(`Resto da divisão: ${a} % ${b} = ${a**b}`);
+
+// incremento = ++
+// decremento = --
+console.log(++a)//11
+console.log(++a)//12
+console.log(--a)//11
+console.log(--a)//10
