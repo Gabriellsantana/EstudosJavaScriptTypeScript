02c8b6d36e08e61e5d3f60e2c9f3919754b73d42
Gabriel <gabrieldevspace@gmail.com>
2024-01-25 23:21:26 -0300

    dados primitivos

---
+++ aula8-dadosPrimitivos/index.js
@@ -0,0 +1,15 @@
+// String , number, undefined , null boolean , symbol
+let nome = `Gabriel`; //String
+let nome1 = "Gabriel"; //String
+let nome2 = 10; //number
+let nome3 = 10.7; // number
+let nome4; // undefined = não aponta para lugar nehum na memória
+let nome5 = null; // undefined = não aponta para lugar nehum na memória
+var boolean = false; // valor lógico
+const aprovado = true;
+const reprovado = false;
+
+console.log(typeof(aprovado))
+console.log(typeof(nome))
+console.log(typeof(nome3))
+console.log(typeof(boolean))
