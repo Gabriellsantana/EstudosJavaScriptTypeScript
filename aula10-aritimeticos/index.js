6d14a59de650a2b96dcf65b66409050e169bffe3
Gabriel <gabrieldevspace@gmail.com>
2024-02-01 00:06:22 -0300

    Promant&Alert&Window

--- aula10-aritimeticos/index.js
+++ aula10-aritimeticos/index.js
@@ -24,3 +24,19 @@ console.log(++a)//11
 console.log(++a)//12
 console.log(--a)//11
 console.log(--a)//10
+
+var contador = 45
+var passo = 2
+contador +=passo // contador = contador + passo
+console.log(`CONTADOR`,contador)
+
+// parseInt (converte o numero para inteiro) parseFloat (converte o numero que era uma string para Real)
+//number (o proprio js indetifica o tipo do dados)
+const num1 = 10;
+const num2 = parseInt("50");
+const num3 = parseFloat("35");
+const num4 = Number("45.98");
+console.log(typeof(num1))
+console.log(typeof(num2))
+console.log(typeof(num3))
+console.log(typeof(num4))
---
+++ aula11-alert&Promant/script.js
@@ -0,0 +1,6 @@
+let num1 = Number(prompt("Digite um Número?"));
+let num2 = Number(prompt("Digite segundo Número"));
+
+const soma = num1+num2;
+
+window.alert(`A soma de ${num1} + ${num2} = ${soma}`)
