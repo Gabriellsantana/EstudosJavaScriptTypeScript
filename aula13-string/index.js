db3d1264225451eed734c32f043fd9492d1cd1e8
Gabriel <gabrieldevspace@gmail.com>
2024-02-02 09:18:18 -0300

    String

---
+++ aula13-string/index.js
@@ -0,0 +1,6 @@
+let UmaString = "é um texto";
+let string = "O rato roeu a roupa do rei de roma"
+console.log(UmaString);
+console.log(UmaString[5]);//aqui estou exebindo apenas uma letra da pavalra no indice
+console.log(string.slice(-5))
+console.log(string.length)
