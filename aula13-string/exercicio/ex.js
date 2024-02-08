faf0cf5dc947b06fb1182fd7ee15f845a5110bee
Gabriel <gabrieldevspace@gmail.com>
2024-02-07 22:40:17 -0300

    exercicio string

---
+++ aula13-string/exercicio/ex.js
@@ -0,0 +1,5 @@
+let nome = prompt("Digite seu nome?")
+window.alert(`Ola ${nome} Sejam-Benvindo`)
+window.document.write(nome)
+
+document.body.style.color = "blue"
--- aula13-string/index.js
+++ aula13-string/index.js
@@ -4,3 +4,5 @@ console.log(UmaString);
 console.log(UmaString[5]);//aqui estou exebindo apenas uma letra da pavalra no indice
 console.log(string.slice(-5))
 console.log(string.length)
+console.log(string.toLocaleUpperCase())// maiscula
+console.log(string.toLocaleLowerCase())// menuscala
