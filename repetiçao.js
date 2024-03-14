bd9602c92b13951811450752b2c9e20d36c21cb0
Gabriel <gabrieldevspace@gmail.com>
2024-03-14 20:42:21 -0300

    estrutura de repetição

---
+++ repetiçao.js
@@ -0,0 +1,28 @@
+
+let n1 = 1;
+let n2 = 2;
+let n3 = 3;
+let n4 = 4;
+let n5 = 5;
+var i = 0 
+var a = 1
+var d = 1
+//para i de 0 ate 10
+for(i = 0; i <=10; i++){
+    console.log(`for ${i} x ${n5} = ${i*n5}`);
+}
+console.log("")
+//enquanto 1 for menor 10   so poder comeca de 1
+while(a <= 10){
+    console.log(`while ${a} x ${n5} = ${a*n5}`);
+    a++;
+}
+console.log("")
+//repita ate 
+do{
+    console.log(`do ${d} x ${n5} = ${d*n5}`);
+    d++;
+} while(d <= 10)
+
+
+
